2990c46e3dc9127749d9a6a9521d8fe529910426
Letícia Rutsatz <leticiarutsatz099@gmail.com>
2025-03-04 15:04:10 -0300

    feat: create db tables and migrations

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Diego Fernandes',
+    email: 'diego@rocketsaeet.com.br',
+  },
+})
 
 export const app = fastify();
