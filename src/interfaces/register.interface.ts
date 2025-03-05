97f39c38c5776e42f0464d97e74b0e2eb1eb8043
Letícia Rutsatz <leticiarutsatz099@gmail.com>
2025-03-05 14:17:06 -0300

    feat: add unitary tests

--- src/interfaces/register.interface.ts
+++ src/interfaces/register.interface.ts
@@ -1,5 +1,10 @@
+import { User } from '@prisma/client'
 export interface RegisterServiceInterface{
     name: string,
     email: string,
     password: string
 }
+
+export interface RegisterUseCaseResponse {
+    user: User
+  }
--- src/services/register.service.ts
+++ src/services/register.service.ts
@@ -1,4 +1,4 @@
-import { RegisterServiceInterface } from '@/interfaces/register.interface';
+import { RegisterServiceInterface, RegisterUseCaseResponse } from '@/interfaces/register.interface';
 import { UsersRepositoryInterface } from '@/interfaces/usersRepository.interface';
 import { hash } from 'bcryptjs';
 import { UserAlreadyExistsError } from './errors/user-already-exists-error';
@@ -23,7 +23,7 @@ export class RegisterService {
         name, 
         email, 
         password 
-    }: RegisterServiceInterface) {
+    }: RegisterServiceInterface) : Promise<RegisterUseCaseResponse> {
         const password_hash = await hash(password, 6);
 
         const userWithSameEmail = await this.usersRepository.findByEmail(email);
@@ -32,11 +32,15 @@ export class RegisterService {
             throw new UserAlreadyExistsError()
         }
 
-        await this.usersRepository.create({
+        const user = await this.usersRepository.create({
             name,
             email,
             password_hash,
         })
+
+        return {
+            user,
+        }
     }
 }
 
