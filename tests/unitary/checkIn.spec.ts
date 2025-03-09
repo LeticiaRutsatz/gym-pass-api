47b420f12cda15c8c109705c4f3ff99b6a0db9ce
Letícia Rutsatz <leticiarutsatz099@gmail.com>
2025-03-09 20:11:54 -0300

    refactor: add coments

--- tests/unitary/checkIn.spec.ts
+++ tests/unitary/checkIn.spec.ts
@@ -196,14 +196,14 @@ describe('CheckIn Use Case', () => {
   })
 
   it('should not be able to validate the check-in after 20 minutes of its creation', async () => {
-    vi.setSystemTime(new Date(2023, 0, 1, 13, 40))
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40)) //set the new date
 
     const createdCheckIn = await checkInRepository.create({
       gym_id: 'gym-01',
       user_id: 'user-01',
     })
 
-    const twentyOneMinutesInMs = 1000 * 60 * 21
+    const twentyOneMinutesInMs = 1000 * 60 * 21 //avance in time
 
     vi.advanceTimersByTime(twentyOneMinutesInMs)
 
