47590cdb70b7718ab05cc21e7e40710ae234ff12
Letícia Rutsatz <leticiarutsatz099@gmail.com>
2025-03-14 08:53:00 -0300

    feat: create tests

--- tests/end-to-end/user-controller.spec.ts
+++ tests/end-to-end/user-controller.spec.ts
@@ -20,4 +20,31 @@ describe('Register (e2e)', () => {
 
     expect(response.statusCode).toEqual(201)
   })
+
+  it('should be able to get user profile', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '12345678',
+    })
+
+    const authResponse = await request(app.server).post('/sessions').send({
+      email: 'johndoe@example.com',
+      password: '12345678',
+    })
+
+    const { token } = authResponse.body
+
+    const profileResponse = await request(app.server)
+      .get('/me')
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    expect(profileResponse.statusCode).toEqual(200)
+    expect(profileResponse.body.user).toEqual(
+      expect.objectContaining({
+        email: 'johndoe@example.com',
+      }),
+    )
+  })
 })
