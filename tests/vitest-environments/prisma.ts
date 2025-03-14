d9b1d60c5be4a64fab637df8bc7a0995a8f7272d
Letícia Rutsatz <leticiarutsatz099@gmail.com>
2025-03-14 08:46:32 -0300

    feat: create a end to end test

--- tests/vitest-environments/prisma.ts
+++ tests/vitest-environments/prisma.ts
@@ -1,14 +1,42 @@
+import 'dotenv/config'
+import { randomUUID } from 'node:crypto'
+import { execSync } from 'node:child_process'
 import { Environment } from 'vitest'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>{
   name: 'prisma',
   transformMode: 'ssr',
-  async setup() { //before tests
-    console.log('Setup')
+  
+  async setup() { //before each test
+    const schema = randomUUID() //change schema of the db to create a separate db enviroment to each test
+    const databaseURL = generateDatabaseURL(schema);
+    console.log('Setup:', databaseURL);
+    
+    process.env.DATABASE_URL = databaseURL;
+    execSync('npx prisma migrate deploy'); //execute a command in terminal
 
     return {
-      async teardown() { //after tests
-        console.log('Teardown')
+      async teardown() { //after each test
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
       },
     }
   },
