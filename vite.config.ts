d8817079b91b20dd0aa6fe3992777081e0431af8
Letícia Rutsatz <leticiarutsatz099@gmail.com>
2025-03-24 20:40:14 -0300

    fix: add secret

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     environmentMatchGlobs: [
-      ['tests/end-to-end/**', './tests/vitest-environments/prisma.ts']
+      ['tests/end-to-end/**', './tests/vitest-environments/prisma.ts'] //saying that the enviroment will run to each test file
     ],
     dir: 'tests'
   }
